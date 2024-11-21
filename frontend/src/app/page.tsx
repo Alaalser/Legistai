0453c6971f7bc02c246c4adfcc940faed0dc06e0
Alaa Alser <alaalsser@gmail.com>
2024-11-22 00:42:24 +0200

    rdirect to registation page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,22 @@
-// src/app/page.tsx
-import React from "react";
+"use client"; // Ensure this is client-side code
 
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation"; // Import useRouter from next/navigation
 
 const HomePage = () => {
-  return (
-    <div>
-      <h1>Welcome to Registration Page</h1>
-    </div>
-  );
+  const router = useRouter();
+
+  useEffect(() => {
+    // Redirect to the register page after 2 seconds
+    const timer = setTimeout(() => {
+      router.push("/register");
+    }, 0);
+
+    // Clean up timer if the component is unmounted
+    return () => clearTimeout(timer);
+  }, [router]);
+
+  return null
 };
 
 export default HomePage;
