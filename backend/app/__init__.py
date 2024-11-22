3b51e467baba6e1e6578c5aef85c82ad0fe25d14
Alaa Alser <alaalsser@gmail.com>
2024-11-22 14:59:34 +0200

    wait for data

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
+from app.db import get_db_connection
 
 
 # Allow all origins (for development purposes)
 def create_app():
     app = Flask(__name__)
     CORS(app)
+    get_db_connection()
 
     # Import and register blueprints
     from .routes import main_routes
